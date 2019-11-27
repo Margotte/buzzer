28ea1651ce1920dedf8a9829b4b589f21e6a2d14
Margotte <margo.tinawi@gmail.com>
2019-11-27 19:58:00 +0100

    alert that works weird

--- public/client.js
+++ public/client.js
@@ -1,2 +1,8 @@
 const connection = new WebSocket('ws://127.0.0.1:8080');
 
+connection.onmessage = (message) => {
+  console.log(message);
+  alert(message.data);
+  const parsed = JSON.parse(data);
+
+}
