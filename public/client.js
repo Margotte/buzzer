649753becbefdf9b69c65daf9432201e2cd44821
Carl von Bonin <carl.vonbonin@gmail.com>
2019-12-04 20:07:53 +0100

    OMG IT WORKS OMG OMG OMG OMG OMGOGMOAMOGMG

--- public/client.js
+++ public/client.js
@@ -1,8 +1,63 @@
 const connection = new WebSocket('ws://127.0.0.1:8080');
 
+team_array = []
+
+const TEAM_COUNT = 6;
+
+function setCardFlipState(index, state) {
+  var card = document.getElementById("card-"+index);
+  if (card.classList.contains("flipped")) {
+    if (state) {
+      return;
+    } else {
+      card.classList.remove("flipped");
+    }
+  } else {
+    if (state) {
+      card.classList.add("flipped");
+    } else {
+      return;
+    }
+  }
+}
+
+function setCardImage(index, image) {
+    var cardImg = document.querySelector("#card-"+index+" .team-img");
+    cardImg.setAttribute("src","images/"+image+".jpg");
+}
+
+function updateFlipCards() {
+  for (var i = 0; i<TEAM_COUNT; i++) {
+    if (i<team_array.length) {
+      setCardImage(i, team_array[i]);
+      setCardFlipState(i, true);
+    } else {
+      setCardFlipState(i, false);
+    }
+  }
+}
+
+function handleParsifiedMessage(parsified) {
+  if (parsified.toggled == true) {
+    if (!team_array.includes(parsified.name)) {
+      team_array.push(parsified.name);
+      updateFlipCards();
+    } else {
+      console.log("Warning: OUT OF SYNC (mais ca va se resync pas de panique)");
+    }
+  } else {
+    if (team_array.includes(parsified.name)) {
+      team_array.splice( team_array.indexOf(parsified.name), 1);
+      updateFlipCards();
+    } else {
+      console.log("Warning: OUT OF SYNC (mais ca va se resync pas de panique)");
+    }
+  }
+} 
+
 connection.onmessage = (message) => {
   console.log(message);
   alert(message.data);
-  const parsed = JSON.parse(data);
-
+  const parsified = JSON.parse(data);
+  handleParsifiedMessage(parsified);
 }
